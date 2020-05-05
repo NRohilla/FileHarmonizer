59f31c24b930954abe6327182dc907c88e53fc98
Shubham Mishra <shubhamkmishra@prodesignsoftdev.onmicrosoft.com>
2020-05-05 14:13:48 +0530

    Chnages for popup

--- Harmonizer.File/Harmonizer.UI/Controllers/FHFileController.cs
+++ Harmonizer.File/Harmonizer.UI/Controllers/FHFileController.cs
@@ -2214,6 +2214,7 @@ namespace Harmonizer.UI.Controllers
                     association.AssocStatus = true;
                     association.AssocCanceledBy = UserID;
                     int createAssociation = _fileData.CreateAssociation(association);
+                    
                     for (int i = 0; i < ds.Tables[0].Rows.Count; i++)
                     {
                         CreateListTemplate objtemp = new CreateListTemplate();
