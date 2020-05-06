fe9b0a900ea4d67759939bc52efebd596d6c1328
Nitin Rohilla <nr19853006@gmail.com>
2020-05-06 12:36:24 +0530

    Created Report data

--- Harmonizer.File/Harmonizer.DB/Data/FHFileData.cs
+++ Harmonizer.File/Harmonizer.DB/Data/FHFileData.cs
@@ -871,53 +871,7 @@ namespace Harmonizer.DB.Data
             return RecordID;
         }
 
-        public List<CostOfOwnership> GetCostOfOwnershipDetails(string FHnumber)
-        {
-            List<CostOfOwnership> lstCostOfOwnership = new List<CostOfOwnership>();
-            con = ConnectionClass.getConnection();
-            SqlCommand cmd = new SqlCommand();
-            try
-            {
-                cmd.Connection = con;
-                cmd.CommandText = "sp_GetCostOfOwnerShipDetails";
-                cmd.CommandType = CommandType.StoredProcedure;
-                cmd.Parameters.Add("@FHnumber", SqlDbType.NVarChar).Value = FHnumber;
-
-                cmd.Parameters.Add("@Action", SqlDbType.NVarChar).Value = "Select";
-                DataSet ds = new DataSet();
-                SqlDataAdapter da = new SqlDataAdapter(cmd);
-                da.Fill(ds);
-                if (ds.Tables.Count > 0 &&
-                    ds.Tables[0].Rows.Count > 0)
-                {
-                    for (int i = 0; i < ds.Tables[0].Rows.Count; i++)
-                    {
-                        CostOfOwnership costOfOwnership = new CostOfOwnership();
-                        costOfOwnership.ID = Convert.ToInt32(ds.Tables[0].Rows[i]["ID"]);
-                        costOfOwnership.FHnumber = Convert.ToString(ds.Tables[0].Rows[i]["FHnumber"]);
-                        costOfOwnership.Associate = Convert.ToString(ds.Tables[0].Rows[i]["Associate"]);
-                        costOfOwnership.DateProcessed = Convert.ToDateTime(ds.Tables[0].Rows[i]["DateProcessed"]);
-                        costOfOwnership.TimeProcessed = (TimeSpan)ds.Tables[0].Rows[i]["TimeProcessed"];
-                        costOfOwnership.TransactionName = Convert.ToString(ds.Tables[0].Rows[i]["TransactionName"]);
-                        costOfOwnership.Msg = Convert.ToString(ds.Tables[0].Rows[i]["Msg"]);
-                        costOfOwnership.TransactionCount = Convert.ToInt32(ds.Tables[0].Rows[i]["Count"]);
-                        lstCostOfOwnership.Add(costOfOwnership);
-                    }
-                }
-                ConnectionClass.closeconnection(con);
-            }
-            catch (Exception ex)
-            {
-                ConnectionClass.closeconnection(con);
-                DataLogger.Write("Report-sp_GetCostOfOwnerShipDetails", ex.Message);
-            }
-            finally
-            {
-                ConnectionClass.closeconnection(con);
-            }
-            return lstCostOfOwnership;
-
-        }
+       
 
     }
 }
